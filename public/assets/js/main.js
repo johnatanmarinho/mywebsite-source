f2e7d7f4155f8a7b0b6f0af18e645566846ce4a1
Johnatan Marinho <johnatan.marinho@gmail.com>
2017-10-31 17:55:27 -0200

    changed the theme colors, the header and the language

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -17,4 +17,48 @@ $(document).ready(function(){
       $(".menu").addClass("menu-opened");
     }
   });
+  
+  $(document).on("scroll", onScroll);
+
+  $('a[href^="#"]').on('click', function (e) {
+    e.preventDefault();
+    $(document).off("scroll");
+
+    $('a').each(function () {
+      $(this).removeClass('active');
+    })
+    $(this).addClass('active');
+
+    if($(".toggle-btn").hasClass("open")){
+      $(".toggle-btn").removeClass("open")
+      $(".menu").removeClass("menu-opened");
+    }
+
+    var target = this.hash,
+        menu = target;
+    $target = $(target);
+    $("html, body").stop().animate({
+      "scrollTop": $target.offset().top+2
+    }, 500, "swing", function () {
+      window.location.hash = target;
+      $(document).on("scroll", onScroll);
+    });
+
+  });
+
 });
+
+function onScroll(event) {
+  var scrollPos = $(document).scrollTop();
+  $(".menu li a").each(function () {
+    var currLink = $(this);
+    var refElement = $(currLink.attr("href"));
+    if(refElement.position().top   <= scrollPos + refElement.height() &&
+        refElement.position().top + refElement.height() > scrollPos){
+      $(".menu li a").removeClass("active");
+      currLink.addClass("active");
+    }else{
+      currLink.removeClass("active");
+    }
+  });
+}
